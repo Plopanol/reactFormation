d138e0065a8b87612fbdaf9a0eac12746637da56
Plopanol <inolivierwetrust@protonmail.ch>
2026-01-13 09:58:25 +0100

    Ajout de la lecture du ficher json avec fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,72 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import InfoPanel from "./components/infoPanel/InfoPanel";
 import MetropolisCard from "./components/metropolisCard/MetropolisCard";
-import data from "./dataLight.json";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(true);
-
+  // Lors du premier rendu du composant,
+  // les données n'ont pas encore été récupées
+  // (temps d'affichage du site < temps de récupération des données)
+  // Nous stockons donc dans notre composant App un état responsable
+  // de la récupération des données des métropoles
+  const [metropolisesDatas, setMetropolisesDatas] = useState({});
+  // Nous nous assurons que la récupération des données ne se fait
+  // qu'au premier chargement du composant App
   useEffect(() => {
-    // ✅ Extrait le tableau du JSON
-    const arrayData = data.items || data || [];
+    const fetchDatas = async () => {
+      const response = await fetch("/eco2mix-metropoles-2025-janvier-01.json");
+      // ‘/’ part du dossier public du projet
+      const datas = await response.json();
+      setMetropolisesDatas(datas);
+    };
 
-    // ✅ Vérifie que c'est un tableau
-    if (Array.isArray(arrayData)) {
-      setItems(arrayData);
-    } else {
-      console.error("Données non valides:", arrayData);
-      setItems([]);
-    }
-
-    setLoading(false);
-  }, []);
+    // attention : il faut utiliser le "setter" 'setMetropolisesDatas' fournit par useState
+    // à ne pas faire : `metropolisesDatas = datas` car on sort complètement de
+    // la logique de useState et du cycle de vie des composants dans React
+    fetchDatas();
+  }, []); // [] => rappel : contenu du useEffect exécuté uniquement
+  // au premier rendu du composant App
 
   return (
     <>
       <header>
         <InfoPanel totalCities="200" avgConsumption="950" period="24" />
       </header>
-      <header>
-        {items.map((item) => (
+      {Object.keys(metropolisesDatas).length !== 0 && (
+        <header>
           <MetropolisCard
-            key={item.id}
-            consommation={item.consommation}
-            nomVille={item.nom}
+            nomVille={Object.keys(metropolisesDatas)[0]}
+            consommation={
+              metropolisesDatas[Object.keys(metropolisesDatas)[0]].datas[
+                "2025-01-01"
+              ]["12:00"].consommation
+            }
           />
-        ))}
-      </header>
+          <MetropolisCard
+            nomVille={Object.keys(metropolisesDatas)[1]}
+            consommation={
+              metropolisesDatas[Object.keys(metropolisesDatas)[1]].datas[
+                "2025-01-01"
+              ]["12:00"].consommation
+            }
+          />
+          <MetropolisCard
+            nomVille={Object.keys(metropolisesDatas)[2]}
+            consommation={
+              metropolisesDatas[Object.keys(metropolisesDatas)[2]].datas[
+                "2025-01-01"
+              ]["12:00"].consommation
+            }
+          />
+        </header>
+      )}
     </>
   );
 }
 
 export default App;
+
+// <MetropolisCard
+//   key={ecoItem.key}
+//   consommation={ecoItem.consommation}
+//   nomVille={ecoItem.nom}
+// />
--- src/components/metropolisCard/MetropolisCard.jsx
+++ src/components/metropolisCard/MetropolisCard.jsx
@@ -2,16 +2,19 @@ import "./MetropolisCard.css";
 import { useState } from "react"; // 1. Import du Hook useState
 
 export default function MetropolisCard({ nomVille, consommation }) {
+  const [isExpanded, setIsExpanded] = useState(false);
   // 2. Déclaration d'une variable d'état 'isExpanded'
   // et de sa fonction de mise à jour 'setIsExpanded'
-  const [isExpanded, setIsExpanded] = useState(false);
+  const handleToggle = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   // Creation du composant "MetropolisCard"
   // MetropolisCard est responsable de l’affichage des données d'une métropole
   return (
     <div className="panel">
-      <h2>{nomVille}</h2>
-      <div className="card-content">
+      <h2 onClick={handleToggle}>{nomVille}</h2>
+      <div className="card-content" hidden={!isExpanded}>
         <p>
           <span className="label">Consommation :</span>
           <span className="value">{consommation} MW</span>
